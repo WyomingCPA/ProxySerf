d0f93e49106081b8b9aa663f8b254856166d2dba
WyomingCPA <WyomingCPA@yandex.ru>
2015-11-21 08:19:04 +0300

    Development

--- ProxySerf/ViewModel/MainViewModel.cs
+++ ProxySerf/ViewModel/MainViewModel.cs
@@ -14,29 +14,6 @@ namespace ProxySerf.ViewModel
         private readonly IDataService _dataService;
 
         /// <summary>
-        /// The <see cref="WelcomeTitle" /> property's name.
-        /// </summary>
-        public const string WelcomeTitlePropertyName = "WelcomeTitle";
-
-        private string _welcomeTitle = string.Empty;
-
-        /// <summary>
-        /// Gets the WelcomeTitle property.
-        /// Changes to that property's value raise the PropertyChanged event. 
-        /// </summary>
-        public string WelcomeTitle
-        {
-            get
-            {
-                return _welcomeTitle;
-            }
-            set
-            {
-                Set(ref _welcomeTitle, value);
-            }
-        }
-
-        /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel(IDataService dataService)
@@ -51,15 +28,8 @@ namespace ProxySerf.ViewModel
                         return;
                     }
 
-                    WelcomeTitle = item.Title;
                 });
         }
 
-        ////public override void Cleanup()
-        ////{
-        ////    // Clean up if needed
-
-        ////    base.Cleanup();
-        ////}
     }
 }
